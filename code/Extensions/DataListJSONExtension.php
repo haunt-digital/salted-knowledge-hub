ce0bad0b7ce0bb52e59c6b2db19e4940ae82f235
Leo Chen <leo@saltedherring.com>
2017-08-20 10:03:43 -1200

    name change

--- code/Extensions/DataListJSONExtension.php
+++ code/Extensions/DataListJSONExtension.php
@@ -1,12 +1,12 @@
 <?php
 
-class DataListExt extends Extension
+class DataListJSONExtension extends Extension
 {
     public function json()
     {
         $lst                =   $this->owner;
         $json_list          =   array();
-        
+
         foreach ($lst as $item) {
             $json_list[]    =  $item->cached(true);
         }
