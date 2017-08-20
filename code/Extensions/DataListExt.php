5bcc08fd1cc3782d6c8e3ab75932cbb8ce681628
Leo Chen <leo@saltedherring.com>
2017-08-20 10:00:12 -1200

    add missing extension

---
+++ code/Extensions/DataListExt.php
@@ -0,0 +1,16 @@
+<?php
+
+class DataListExt extends Extension
+{
+    public function json()
+    {
+        $lst                =   $this->owner;
+        $json_list          =   array();
+        
+        foreach ($lst as $item) {
+            $json_list[]    =  $item->cached(true);
+        }
+
+        return $json_list;
+    }
+}
