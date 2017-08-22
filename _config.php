52d857c3f2a78f00a127b4bd4ead0e9a6cc582ff
Leo Chen <leo@saltedherring.com>
2017-08-22 11:41:17 -1200

    putting cache factory in

--- _config.php
+++ _config.php
@@ -1,2 +1,5 @@
 <?php
 define('HUB_DIR', basename(dirname(__FILE__)));
+
+SS_Cache::set_cache_lifetime('KnowledgeArticle', 31536000); // 1 year
+SS_Cache::set_cache_lifetime('KnowledgeCategoryList', 31536000); // 1 year
