8875f7448330b15be5a5e969d9396fecf01ba536
Leo Chen <leo@saltedherring.com>
2017-08-22 00:07:46 -1200

    added pagehero

---
+++ code/Extensions/KnowledgeArticleHeroExtension.php
@@ -0,0 +1,18 @@
+<?php
+
+class KnowledgeArticleHeroExtension extends DataExtension
+{
+    private static $has_one = array(
+        'PageHero'      =>  'Image'
+    );
+
+    public function updateCMSFields(FieldList $fields)
+    {
+        $fields->addFieldToTab(
+            'Root.Main',
+            class_exists('SaltedUploader') ?
+            SaltedUploader::create('PageHero', 'Page hero image')->setCropperRatio(16/9) :
+            UploadField::create('PageHero', 'Page hero image')
+        );
+    }
+}
