54ff1422943f614cce13b4d174a455764d7979f7
Leo Chen <leo@saltedherring.com>
2017-09-05 23:54:01 -1200

    Adding case study

---
+++ code/Layout/CaseStudyKnowledgeArticle.php
@@ -0,0 +1,12 @@
+<?php
+
+class CaseStudyKnowledgeArticle extends KnowledgeArticle
+{
+    private static $singular_name   =   'Case study';
+    private static $plural_name     =   'Case studies';
+}
+
+class CaseStudyKnowledgeArticle_Controller extends KnowledgeArticle_Controller
+{
+
+}
