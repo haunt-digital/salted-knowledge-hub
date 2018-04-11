2e2580af6139ec72bad8b455b2354c4dfe4e3360
Simon Winter <simon@saltedherring.com>
2018-04-11 18:30:07 +1200

    Update GenericKnowledgeArticle.php

--- code/Layout/GenericKnowledgeArticle.php
+++ code/Layout/GenericKnowledgeArticle.php
@@ -4,6 +4,7 @@ class GenericKnowledgeArticle extends KnowledgeArticle
 {
     private static $singular_name   =   'Knowledge article';
     private static $plural_name     =   'Knowledge articles';
+    private static $hide_ancestor   =   'KnowledgeArticle';
 }
 
 class GenericKnowledgeArticle_Controller extends KnowledgeArticle_Controller
